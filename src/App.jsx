eab275cf545790345dd80b75d6c9215352718f54
AdityaDinata <adityadinata647@gmail.com>
2024-12-02 04:37:35 +0800

    forgotpass belum fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,29 @@
-import AppRouter from "./routes/AppRoutes"
+import { RouterProvider, createBrowserRouter, Navigate } from "react-router-dom";
+import Register from "./pages/Auth/Register";
+import Login from "./pages/Auth/Login";
+import ForgotPass from "./pages/Auth/forgotPass";
+
+const routes = [
+  { path: "/", element: <Navigate to="/login" replace /> },
+  { path: "/register", element: <Register /> },
+  { path: "/login", element: <Login /> },
+  { path: "/aturulang", element: <ForgotPass /> },
+  { path: "*", element: <div>404 - Page Not Found</div> },
+];
+
+const router = createBrowserRouter(routes, {
+  future: {
+    v7_relativeSplatPath: true,
+    v7_startTransition: true,
+    v7_fetcherPersist: true,
+    v7_normalizeFormMethod: true,
+    v7_partialHydration: true,
+    v7_skipActionErrorRevalidation: true,
+  },
+});
+
 function App() {
-  return (
-    <AppRouter />
-  )
+  return <RouterProvider router={router} />;
 }
 
-export default App
+export default App;
--- src/routes/public/PublicRoutes.jsx
+++ src/routes/public/PublicRoutes.jsx
@@ -7,9 +7,6 @@ import ResetPassword from "../../pages/Auth/resetPassword";
 const PublicRoutes = () => {
   return (
     <Routes>
-      
-      {/* <Route path="/" element={<Register />} /> */}
-
       <Route path="/reset" element={<ResetPassword />} />
       <Route path="/aturulang" element={<ForgotPass />} />
       <Route path="/verify" element={<VerifyCode />} />
